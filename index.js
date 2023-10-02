d9ac2350f6a09fe0a73c9195ffce50b2f6329a3c
surendra singh <123186007+kewat1995@users.noreply.github.com>
2023-10-02 04:05:50 +0100

    URL path Update index.js

--- index.js
+++ index.js
@@ -5,49 +5,49 @@ const projects = [
     {
         image :"image/project1.png",
         title:"APj abdul kalam",
-        url:"../abdulkhalam/index.html"
+        url:"./abdulkhalam/index.html"
 
     },
     {
         image :"image/project2.png",
         title:"job Application",
-        url:"../Form/index.html"
+        url:"./Form/index.html"
 
     },
     {
         image :"image/project3.png",
         title:"parallex website",
-        url:"../parallex/index.html"
+        url:"./parallex/index.html"
 
     },
     {
         image :"image/project4.png",
         title:"developer page",
-        url:"../developerpage/index.html"
+        url:"./developerpage/index.html"
 
     },
     {
         image :"image/project5.png",
         title:"Resturent-website",
-        url:"../Resturent-website/index.html"
+        url:"./Resturent-website/index.html"
 
     },
     {
         image :"image/project6.png",
         title:"Music website",
-        url:"../musicwebsite/index.html"
+        url:"./musicwebsite/index.html"
 
     },
     {
         image :"image/project7.png",
         title:"youtube clone",
-        url:"../youtube-clone/index.html"
+        url:"./youtube-clone/index.html"
 
     },
     {
         image :"image/project8.png",
         title:"Document",
-        url:"../js document/index.html"
+        url:"./js document/index.html"
 
     },
     
